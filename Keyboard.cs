005b3613d20b635120331644a19f41e4002ca3df
Sam <sam.lin@qima.com>
2023-07-27 19:21:21 +0800

    init

---
+++ Keyboard.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AlwaysAvailable;
+
+public class Keyboard
+{
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+    const int KEY_UP_EVENT = 0x0002; //Key up flag
+
+    public static void KeyUp(byte key)
+    {
+        keybd_event(key, 0, KEY_UP_EVENT, 0);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AlwaysAvailable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Keyboard.KeyUp(131);
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
