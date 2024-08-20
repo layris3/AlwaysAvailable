323c9388e95b3984c9233c521139a999b67c12be
sam <lingourui@gmail.com>
2024-08-20 14:34:49 +0800

    add keyboard logic

--- Program.cs
+++ Program.cs
@@ -4,10 +4,37 @@
     {
         static void Main(string[] args)
         {
+            bool runForever = false;
+            int hour = 0;
+            int min = 0;
+            Console.WriteLine("please enter an hour, or enter 'n' to always run.");
+            String hourStr = Console.ReadLine();
+            if (hourStr == "n" || hourStr == "N")
+                runForever = true;
+            else
+            {
+                Console.WriteLine("please enter the mininute.");
+                String minStr = Console.ReadLine();
+                try
+                {
+                    hour = int.Parse(hourStr);
+                    min = int.Parse(minStr);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("invalid hour and minute,exiting...");
+                    throw;
+                }
+            }
+
             while (true)
             {
                 Keyboard.KeyUp(131);
                 Thread.Sleep(1000);
+                if (!runForever && DateTime.Now.Hour >= hour && DateTime.Now.Minute >= min)
+                {
+                    break;
+                }
             }
 
         }
